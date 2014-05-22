552200eeaeec583d761e2ed4d8117a00b5cfb2fc
Mark Powell <drmarkpowell@gmail.com>
2014-05-22 12:00:43 -0700

    Quaternion model class.

---
+++ Mars-Images/Quaternion.h
@@ -0,0 +1,18 @@
+//
+//  Quaternion.h
+//  Mars-Images
+//
+//  Created by Mark Powell on 5/21/14.
+//  Copyright (c) 2014 Powellware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Quaternion : NSObject
+
+@property (nonatomic) double w;
+@property (nonatomic) double x;
+@property (nonatomic) double y;
+@property (nonatomic) double z;
+
+@end
---
+++ Mars-Images/Quaternion.m
@@ -0,0 +1,13 @@
+//
+//  Quaternion.m
+//  Mars-Images
+//
+//  Created by Mark Powell on 5/21/14.
+//  Copyright (c) 2014 Powellware. All rights reserved.
+//
+
+#import "Quaternion.h"
+
+@implementation Quaternion
+
+@end
