5bc0dafcd752ae7e2d6ad0224ae7e83f8f9cc828
Mark Powell <drmarkpowell@gmail.com>
2014-12-29 14:28:43 -0800

    removed commented line of code that was superfluous

--- Mars-Images/ImageUtility.m
+++ Mars-Images/ImageUtility.m
@@ -12,7 +12,6 @@
 @implementation ImageUtility
 
 + (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize {
-    //UIGraphicsBeginImageContext(newSize);
     // In next line, pass 0.0 to use the current device's pixel scaling factor (and thus account for Retina resolution).
     // Pass 1.0 to force exact pixel size.
     UIGraphicsBeginImageContextWithOptions(newSize, NO, 1.0);
