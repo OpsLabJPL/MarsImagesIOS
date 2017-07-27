1bb22daff83bdc9eb4594345b95d6f5a68292fd1
Mark Powell <Mark.W.Powell@jpl.nasa.gov>
2017-07-27 14:02:55 -0700

    creating new project

---
+++ Mars-Images/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mars-Images
+//
+//  Created by Mark Powell on 7/27/17.
+//  Copyright © 2017 Mark Powell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
