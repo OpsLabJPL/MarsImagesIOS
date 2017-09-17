dc89b488aba38d7e25bb259088d55bec8a920ad0
Mark Powell <Mark.W.Powell@jpl.nasa.gov>
2017-09-17 11:17:48 -0700

    comment

--- MarsImagesIOS/ImageUtility.swift
+++ MarsImagesIOS/ImageUtility.swift
@@ -47,7 +47,7 @@ class ImageUtility {
         filter.setValue(ciImage, forKey: kCIInputBackgroundImageKey)
         let outputImage = filter.outputImage!
 
-        //TODO had a crash here once. Watch for it again.
+        //TODO had a crash here twice, tinting the left eye image. Watch for it again. Only in the debugger?
         let cgimg = context.createCGImage(outputImage, from: outputImage.extent)!
         return UIImage(cgImage: cgimg)
     }
