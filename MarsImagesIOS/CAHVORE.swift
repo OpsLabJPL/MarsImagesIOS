4d9a9ccf7e71751e9805596cf86b26afa3d63bf7
Mark Powell <Mark.W.Powell@jpl.nasa.gov>
2017-09-29 11:48:38 -0700

    implemented CAHVOR mapping properly, CAHVORE still TODO

--- MarsImagesIOS/CAHVORE.swift
+++ MarsImagesIOS/CAHVORE.swift
@@ -14,6 +14,12 @@ class CAHVORE: CAHVOR {
     var p:Double = 0.0
     
     override func cmod_2d_to_3d(pos2: [Double], pos3: inout [Double], uvec3: inout [Double]) {
+        return super.cmod_2d_to_3d(pos2: pos2, pos3: &pos3, uvec3: &uvec3)
+        //TODO
+    }
+    
+    override func cmod_3d_to_2d(pos3: [Double], range: inout [Double], pos2: inout [Double]) {
+        return super.cmod_3d_to_2d(pos3: pos3, range: &range, pos2: &pos2)
         //TODO
     }
 }
