5180fad29a83204de41b158361deebd5736a2d59
buddybuild <help@buddybuild.com>
2017-11-16 15:38:39 -0800

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- MarsImagesIOS/AppDelegate.swift
+++ MarsImagesIOS/AppDelegate.swift
@@ -21,6 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        BuddyBuildSDK.setup()
+        
         // Override point for customization after application launch.
         
         if let options = launchOptions {
